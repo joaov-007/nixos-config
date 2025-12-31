84cad21ac7fcfc087732efb4c04a2a58f02b41f6
joaov <79527341+joaov-007@users.noreply.github.com>
2025-12-31 10:57:35 -0300

    +develop: saving overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,14 +4,14 @@
   # When applied, the stable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
   stable-packages = final: prev: {
-    stable = import inputs.nixpkgs {
+    stable = import inputs.nixpkgs-stable {
       system = final.system;
       config.allowUnfree = true;
     };
   };
 
-  nightly-packages = final: prev: {
-    nightly = import inputs.nixpkgs-nightly {
+  unstable-packages = final: prev: {
+    unstable = import inputs.nixpkgs {
       system = final.system;
       config.allowUnfree = true;
     };
