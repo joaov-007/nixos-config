ecf54e3c8e4e06688e188838a4cd8e778937d75c
joaov-007 <9527341+joaov-007@users.noreply.github.com>
2026-01-19 18:11:49 -0300

    add: nix config

---
+++ modules/system/nix/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  config = {
+    nix = {
+      package = pkgs.lix;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+      settings = {
+        substituters = [
+          "https://cache.nixos.org"
+          "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+        trusted-users = ["@wheel"];
+        auto-optimise-store = true;
+      };
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
+
+    programs.nix-ld = {
+      enable = true;
+      #Include libstdc++ in the nix-ld profile
+      libraries = [
+        pkgs.stdenv.cc.cc
+        pkgs.zlib
+        pkgs.fuse3
+        pkgs.icu
+        pkgs.nss
+        pkgs.openssl
+        pkgs.curl
+        pkgs.expat
+        pkgs.xorg.libX11
+        pkgs.vulkan-headers
+        pkgs.vulkan-loader
+        pkgs.vulkan-tools
+      ];
+    };
+    system.stateVersion = "25.05";
+  };
+}
