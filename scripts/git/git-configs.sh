f278f8b1f4bfa17a5224fa1ee0ab95b3e11e4a52
joaov-007 <9527341+joaov-007@users.noreply.github.com>
2026-01-20 10:29:07 -0300

    set: now I add opts shell array for git

--- scripts/git/git-configs.sh
+++ scripts/git/git-configs.sh
@@ -8,6 +8,28 @@ setgit() {
 }
 
 git_options=(
+
+    branch.autosetuprebase always
+
+    fetch.prune true
+
+    pull.default current
+
+    pull.rebase true
+
+    push.autoSetupRemote true
+
+    push.default current
+
+    rebase.autoSquash true
+
+    rebase.autoStash true
+
+    rebase.stat true
+
+    rerere.autoUpdate true
+
+    rerere.enabled true
 )
 
 setgit "${git_options[@]}"
