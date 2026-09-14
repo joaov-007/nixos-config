790242687ecfb110007c4f3fcc2dfd31177e7095
joaov <9527341+joaov-007@users.noreply.github.com>
2026-09-14 20:18:26 -0300

    feat(assertion): add assertion to enforce the pipewire
    is simple but enforce the behaviors

--- modules/system/audio/default.nix
+++ modules/system/audio/default.nix
@@ -4,25 +4,39 @@
   ...
 }: {
   flake.nixosModules.audio = {
+    config,
     pkgs,
     lib,
     ...
   }: {
-    environment.systemPackages = with pkgs; [
-      playerctl
-      pavucontrol
-    ];
+    config = {
+      assertions = [
+        {
+          assertion = !config.services.pulseaudio.enable;
+          message = "pulseaudio must be disabled (pipewire replaces it)";
+        }
+        {
+          assertion = config.services.pipewire.enable;
+          message = "pipewire must be enabled";
+        }
+      ];
 
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      audio.enable = true;
-      pulse.enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      jack.enable = true;
-      wireplumber.enable = true;
+      environment.systemPackages = with pkgs; [
+        playerctl
+        pavucontrol
+      ];
+
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        audio.enable = true;
+        pulse.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        jack.enable = true;
+        wireplumber.enable = true;
+      };
     };
   };
 }
