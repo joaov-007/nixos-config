ba20ca5b2c2e73cbc3f0e26cadef77acf2a13bb1
joaov <9527341+joaov-007@users.noreply.github.com>
2026-09-18 09:57:35 -0300

    fix(passwd): I set wrong the passwold

--- modules/system/core/user.nix
+++ modules/system/core/user.nix
@@ -12,10 +12,10 @@
     users.mutableUsers = false;
 
 
-    users.users.root.hashedPassword = "$y$j9T$GWSRZO7Ei.3cF4vc3pA1A/$cl8hJ8hbSO6ATanQYmqYxtfF5kilmQj/l3Ax/48Cot/";
+    users.users.root.hashedPassword = "$y$j9T$wlXYjqg/QqlcSvrY9yByV/$72cAizf8yT0Pw6NPLvt/kaQ8BpXQoZ0VEVAA/fXCsU5";
     users.users.joaov = {
       isNormalUser = true;
-      hashedPassword = "$y$j9T$JGNdjh2bclSh3Hxhle814.$cYDwdbVp./b.n3.OavtJYF./7IUjyRXSVW1howLANo.";
+      hashedPassword = "$y$j9T$wlXYjqg/QqlcSvrY9yByV/$72cAizf8yT0Pw6NPLvt/kaQ8BpXQoZ0VEVAA/fXCsU5";
       extraGroups = ["wheel" "networkmanager" "render" "input" "adm" "audio" "video" "dialout" "plugdev" "i2c" "fuse" "dialout" "gpio" "spi" "netdev" "games" "cdrom"];
       subUidRanges = [
         {
